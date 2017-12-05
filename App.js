56a8bf70a2bac3572722c718808296c4935ab969
dkijc <wwjdat2000@yahoo.com>
2017-12-05 10:50:30 -0500

    added rest client

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
-import App from "./app/index";
+import App from "./src/index";
+import express from 'express';
+
 export default App;
+
+// Initialize http server
+const app = express();
+
+// Handle / route
+app.get('/', (req, res) =>
+  res.send('Hello World!')
+)
+
+// Launch the server on port 3000
+const server = app.listen(3000, () => {
+  const { address, port } = server.address();
+  console.log(`Listening at http://${address}:${port}`);
+});
 
