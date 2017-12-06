111c847f1847135571c10cecfbd039823ba7960a
dkijc <wwjdat2000@yahoo.com>
2017-12-05 21:39:53 -0500

    commit
    removed api stuff

--- App.js
+++ App.js
@@ -1,19 +1,2 @@
 import App from "./src/index";
-import express from 'express';
-
 export default App;
-
-// Initialize http server
-const app = express();
-
-// Handle / route
-app.get('/', (req, res) =>
-  res.send('Hello World!')
-)
-
-// Launch the server on port 3000
-const server = app.listen(3000, () => {
-  const { address, port } = server.address();
-  console.log(`Listening at http://${address}:${port}`);
-});
-
